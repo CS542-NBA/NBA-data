6f137c322ec155622a1a4bcd2e4ba194ee0e15d8
Jiaming Di <842603006@qq.com>
2018-04-15 16:14:11 -0400

    add radarChart()

--- server/client/js/plot.js
+++ server/client/js/plot.js
@@ -1,24 +1,26 @@
-function barplot(playerName, stat, div_id) {
+function barplot(data, div_id='123', statName='Scores') {
     var data = [
         {
-            x: playerName,
-            y: stat,
+            x: data[0],
+            y: data[1],
             type : 'bar',
             marker: {
-                color: 'rgb(100, 200, 300)'
+                color: 'rgba(58,200,225,.5)'
             }
         }
     ];
-
-    var layout = {title: 'NBA STAT BARPLOT'};
+    var title = 'NBA Top 5 ' + statName
+    var layout = {title: title};
     return Plotly.newPlot(div_id, data, layout)
 }
 
-function pieChart(id, values) {
+
+
+function pieChart(data=[60, 21], div_id='123', labels=['Win', 'Loss']) {
     var data= [
         {
-            values: values,
-            labels: ['Win', 'Loss'],
+            values: data,
+            labels: labels,
             type: 'pie'
         }
     ];
@@ -28,5 +30,25 @@ function pieChart(id, values) {
         width: 500
     };
 
-    return  Plotly.newPlot(id, data, layout);
+    return  Plotly.newPlot(div_id, data, layout);
+}
+
+function radarChart(data=[0.5, 0.6, 0.5, 0.2, 0.3], div_id='123'){
+    data = [{
+        type: 'scatterpolar',
+        r: data,
+        theta: ['Scores', 'Rebound', 'Assist', 'Steal', 'Block'],
+        fill: 'toself'
+    }]
+
+    var layout = {
+        polar: {
+            radialaxis: {
+                visible: true,
+                range: [0, 20]
+            }
+        },
+        showlegend: false
+    }
+    return Plotly.newPlot(div_id, data, layout)
 }
